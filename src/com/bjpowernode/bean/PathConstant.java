7e5e3e9f1cab3b238e3aff56ca2d4176a93dbc84
ZhangShuang <vindinser@gmail.com>
2023-12-25 09:47:35 +0800

    完善用户初始化

---
+++ src/com/bjpowernode/bean/PathConstant.java
@@ -0,0 +1,8 @@
+package com.bjpowernode.bean;
+
+// 路径常量
+
+public class PathConstant {
+  // 初始化用户存放路径
+  public static final String USER_PATH = "user/user.txt";
+}
--- src/com/bjpowernode/until/InitDataUtil.java
+++ src/com/bjpowernode/until/InitDataUtil.java
@@ -1,5 +1,7 @@
 package com.bjpowernode.until;
 
+import com.bjpowernode.bean.Constant;
+import com.bjpowernode.bean.PathConstant;
 import com.bjpowernode.bean.User;
 
 import java.io.File;
@@ -23,7 +25,7 @@ public class InitDataUtil {
             // 创建文件夹
             File directory = new File("user/");
             // 创建文件
-            File file = new File("user/user.txt");
+            File file = new File(PathConstant.USER_PATH);
             // 判断文件夹是否存在
             if(!directory.exists()) {
                 directory.mkdir();
@@ -34,12 +36,12 @@ public class InitDataUtil {
                 List<User> list = new ArrayList<>();
                 // 判断userList是否为空
                 if(userList == null) {
-                    list.add(new User(1001, "张大虎", "正常", BigDecimal.TEN));
+                    list.add(new User(1001, "张大虎", Constant.USER_OK, BigDecimal.TEN));
                 } else {
                     list = userList;
                 }
                 // 利用对象输出流将list数据写出到文件中
-                oos = new ObjectOutputStream(new FileOutputStream("user/user.txt"));
+                oos = new ObjectOutputStream(new FileOutputStream(PathConstant.USER_PATH));
                 oos.writeObject(list);
             }
         } catch (IOException e) {
