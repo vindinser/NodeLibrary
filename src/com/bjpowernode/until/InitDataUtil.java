4c936f68037733f91a5f29885c3ffe8d09c854df
ZhangShuang <vindinser@gmail.com>
2023-12-30 13:21:10 +0800

    优化初始化方法

--- src/com/bjpowernode/until/InitDataUtil.java
+++ src/com/bjpowernode/until/InitDataUtil.java
@@ -15,8 +15,54 @@ import java.util.List;
 
 public class InitDataUtil {
     public static void main(String[] args) {
-        initUser(null);
-        initBook(null);
+        // initUser(null);
+        // initBook(null);
+
+        // 初始化用户数据
+        List<User> userList = new ArrayList<>();
+        userList.add(new User(1001, "张大虎", Constant.USER_OK, BigDecimal.TEN));
+        initData(PathConstant.USER_PATH, userList);
+
+        // 初始化图书数据
+        List<Book> bookList = new ArrayList<>();
+        bookList.add(new Book(1, "java零基础入门", "老杜", Constant.TYPE_COMPUTER, "123-1", "动力节点出版社", Constant.STATUS_STORAGE));;
+        initData(PathConstant.BOOK_PATH, bookList);
+    }
+
+    /**
+     * 初始化数据
+     * @param path 写入文件路径
+     * @param list 写入文件内容
+     */
+    public static void initData(String path, List<?> list) {
+        ObjectOutputStream oos = null;
+        try {
+            // 创建文件夹
+            File directory = new File(path.split("/")[0] + "/");
+            // 创建文件
+            File file = new File(path);
+            // 判断文件夹是否存在
+            if(!directory.exists()) {
+                directory.mkdir();
+            }
+            // 判断文件是否存在
+            if(!file.exists()) {
+                file.createNewFile();
+                // 利用对象输出流将list数据写出到文件中
+                oos = new ObjectOutputStream(new FileOutputStream(path));
+                oos.writeObject(list);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if(oos != null) {
+                try {
+                    oos.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 
     // 初始化用户数据
@@ -81,7 +127,7 @@ public class InitDataUtil {
                     list = bookList;
                 }
                 // 利用对象输出流将list数据写出到文件中
-                oos = new ObjectOutputStream(new FileOutputStream(PathConstant.USER_PATH));
+                oos = new ObjectOutputStream(new FileOutputStream(PathConstant.BOOK_PATH));
                 oos.writeObject(list);
             }
         } catch (IOException e) {
